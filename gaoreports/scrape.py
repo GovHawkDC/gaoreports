6bbbc315a07abbf7b0d805c54aad14260ec8b374
Tim Showers <showerst@gmail.com>
2025-11-13 15:13:19 -0500

    Update scraper to with more accurate headers

--- gaoreports/scrape.py
+++ gaoreports/scrape.py
@@ -6,6 +6,7 @@ import lxml.html
 import os
 import re
 import requests
+import time
 
 logging.basicConfig(level=logging.INFO)
 
@@ -94,6 +95,7 @@ def process_item(item: lxml.html.HtmlElement) -> None:
 
     save(output)
 
+    time.sleep(0.15)
 
 def save(output):
     date = datetime.datetime.strptime(output["published"], "%Y-%m-%dT%H:%M:%SZ")
@@ -119,16 +121,33 @@ def search_page(start: str, end: str, page_number: int) -> bool:
     start = round(datetime.datetime.timestamp(start))
     end = round(datetime.datetime.timestamp(end))
 
+    headers = {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-language': 'en-US,en;q=0.9',
+        'cache-control': 'no-cache',
+        'dnt': '1',
+        'pragma': 'no-cache',
+        'priority': 'u=0, i',
+        'sec-ch-ua': '"Not;A=Brand";v="99", "Google Chrome";v="139", "Chromium";v="139"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Linux"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'none',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36',
+    }
     params = {
         "page": page_number,
         "f[0]": f"rt_date_range_gui:(min:{start},max:{end})",
     }
 
     logging.info("Fetching page %d", page_number)
-    response = requests.get(search_url, params=params)
+    response = requests.get(search_url, params=params, headers=headers)
 
     if response.status_code != 200:
-        logging.error(f"Got HTTP {response.status_code} response code, ending scrape")
+        logging.error(f"Got HTTP {response.status_code} response code, {response.url} ending scrape")
         return False
 
     page = lxml.html.fromstring(response.content)
